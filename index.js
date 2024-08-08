edb03582cfb52378ebbfa782f0fba93ceaa4ef69
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 11:43:28 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,81 @@
+// FUNCTION 1: Welcomer
+// Define a function called greeting that takes a name as a parameter and console logs "Hello " + name
+
+// function greeting(name){
+//   console.log("Hello", name)
+// }
+
+// // Call the greeting function with your name as the argument
+// // greeting("Fatou");
+
+// // Prompt the user to enter their name
+// let Username = prompt("Enter your name:");
+
+// // Call the greeting function and pass in the user's name as an argument.
+
+// greeting(Username);
+
+// // -------------------------------------------------
+// // FUNCTION 2: Adder
+// // Define a function called add that takes two numbers as parameters and returns the sum
+
+// function add(num1, num2){
+//   let sum = num1 + num2
+//   return sum
+// }
+
+
+// // Call the add function and pass in any two numbers to test. Console log it (either directly or through a variable)! 
+
+// console.log(add(5,4))
+
+// // Prompt the user to enter one number
+// let number1 = prompt("Enter a number")
+// number1 = parseInt(number1)
+// // Prompt the user to enter another number
+// let number2 = prompt("Enter another number")
+// number2 = parseInt(number2)
+// // Call the add function and pass in the user's numbers. Console log it (either directly or through a variable)! 
+
+// console.log(add(number1, number2))
+
+
+// Notice how the value that printed just put the numbers next to each other instead of adding them. That's because they're both strings!
+// Use parseInt() on both of the user's numbers to make them into numbers (you can re-assign existing variables or declare new ones)
+
+//did that above
+
+// Call the add function with the parsed number values from the user. Console log it and check that the math works correctly!
+
+
+//did that above^^
+
+
+
+
+// -------------------------------------------------
+// FUNCTION 3: Calculator (Extra Credit)
+// Define a function called takes in 3 parameters: operation, number1, and number2
+// The function should use if/else if/else statements on the operation parameter to perform the correct calculation on the two numbers
+// You should support "add", "subtract", "multiple", and "divide" operations
+
+function math(operation, number1, number2){
+  if(){
+    
+  }
+}
+
+
+// Test it out with at least 2 different functions calls
+
+
+
+
+
+
+
+
+
+
+
+
